c46b0a36f59b8954921f27b1ac37bbe94be95760
theeshyguy <t.siopes00@yahoo.com>
2023-03-06 14:57:16 -0500

    updated topics and collections

--- client/src/pages/CollectionNav.js
+++ client/src/pages/CollectionNav.js
@@ -7,20 +7,29 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 
 import { QUERY_COLLECTION_ALL } from "../utils/queries";
 
-function CollectionNavigation () {
+function Collections () {
     let { loading, data } = useQuery(QUERY_COLLECTION_ALL);
 
     if(loading) return <span>Loading...</span>
 
-    const collectionList = data.collections;
+    let collectionList = data.collections.map((item) => 
+        Object.assign({}, item, {selected:false})
+    )
 
-    let sortedList = []
+    if(!collectionList || !collectionList[0]) return <span>Error</span>
 
-    for(let index of collectionList) {
-        sortedList.push(index)
+    collectionList = collectionList.sort((a, b) => a.sortedName.localeCompare(b.sortedName));
+
+    for(let n = 0; n < collectionList.length; ++n) {
+        collectionList[n].firstLetter = false;
+        if(n===0) {
+            collectionList[n].firstLetter = true;
+        } else {
+            if(collectionList[n].sortedName.charAt(0) !== collectionList[n-1].sortedName.charAt(0)) {
+                collectionList[n].firstLetter = true;
+            }
+        }
     }
-
-    sortedList = sortedList.sort((a,b) => a.name.localeCompare(b.name));
 
     const searchFunction = () => {
         let input, filter, group, elements, body, textValue;
@@ -42,25 +51,65 @@ function CollectionNavigation () {
             <MetaTags>
                 <title>1001 Nuggets - Collections</title>
             </MetaTags>
-            <Card>
-            <Card.Header><Link className="link-theme" to={`/`}>Home</Link> {`>`} Collections</Card.Header>
-                <Card.Header className="text-center py-3">
-                    <Card.Title>Collections</Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <input type="text" id="myInput" onKeyUp={searchFunction} placeholder="Search for collection names..." className="mb-3"/>
-                    <Row id="myGroup">
-                        {sortedList.map((index) => (
-                            <Col xs={12} sm={6} md={4} lg={3} key={index.name} className="text-center mb-3">
-                                <span><Link to={`/Collection/${index.realID}`} className="link-theme">{index.name}</Link></span>
-                                <span className="subtext"> {`(`} {index.quotes.length} {index.quotes.length === 1 ? "quote" : "quotes"} {`)`}</span>
-                            </Col>
-                        ))}
-                    </Row>
-                </Card.Body>
-            </Card>
+            <Row className="justify-content-center navPage-body">
+                <Col>
+                    <Card>
+                        <Card.Header><Link className="link-theme" to={`/`}>Home</Link> {`>`} Collections</Card.Header>
+                        <Card.Body>
+                            <Row className="d-none d-md-block">
+                                <Col><input type="text" id="myInput" onKeyUp={searchFunction} placeholder="Search for collection names..." className="mb-3"/></Col>
+                            </Row>
+                            <Row id="myGroup">
+                                {collectionList.map((index) => (
+                                    <Col xs={12} key={index.name} className="text-center mb-2">
+                                        {index.firstLetter &&
+                                            <div>
+                                                <strong><p id={index.sortedName.charAt(0)}>{index.sortedName.charAt(0)}</p></strong>
+                                                <hr/>
+                                            </div>
+                                        }
+                                        <p><strong><Link to={`/collection/${index.realID}`} className="link-theme">{index.name}</Link></strong></p>
+                                        <p className="subtext"> {`(`} {index.quotes.length} {index.quotes.length === 1 ? "quote" : "quotes"} {`)`}</p>
+                                        <hr/>
+                                    </Col>
+                                ))}
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <div className="col-05 p-0 text-center">
+                    <div className="fitter py-2 px-1">
+                        <p className="my-0"><a href="#A" className="sidebar-text text-white">A</a></p>
+                        <p className="my-0"><a href="#B" className="sidebar-text text-white">B</a></p>
+                        <p className="my-0"><a href="#C" className="sidebar-text text-white">C</a></p>
+                        <p className="my-0"><a href="#D" className="sidebar-text text-white">D</a></p>
+                        <p className="my-0"><a href="#E" className="sidebar-text text-white">E</a></p>
+                        <p className="my-0"><a href="#F" className="sidebar-text text-white">F</a></p>
+                        <p className="my-0"><a href="#G" className="sidebar-text text-white">G</a></p>
+                        <p className="my-0"><a href="#H" className="sidebar-text text-white">H</a></p>
+                        <p className="my-0"><a href="#I" className="sidebar-text text-white">I</a></p>
+                        <p className="my-0"><a href="#J" className="sidebar-text text-white">J</a></p>
+                        <p className="my-0"><a href="#K" className="sidebar-text text-white">K</a></p>
+                        <p className="my-0"><a href="#L" className="sidebar-text text-white">L</a></p>
+                        <p className="my-0"><a href="#M" className="sidebar-text text-white">M</a></p>
+                        <p className="my-0"><a href="#N" className="sidebar-text text-white">N</a></p>
+                        <p className="my-0"><a href="#O" className="sidebar-text text-white">O</a></p>
+                        <p className="my-0"><a href="#P" className="sidebar-text text-white">P</a></p>
+                        <p className="my-0"><a href="#Q" className="sidebar-text text-white">Q</a></p>
+                        <p className="my-0"><a href="#R" className="sidebar-text text-white">R</a></p>
+                        <p className="my-0"><a href="#S" className="sidebar-text text-white">S</a></p>
+                        <p className="my-0"><a href="#T" className="sidebar-text text-white">T</a></p>
+                        <p className="my-0"><a href="#U" className="sidebar-text text-white">U</a></p>
+                        <p className="my-0"><a href="#V" className="sidebar-text text-white">V</a></p>
+                        <p className="my-0"><a href="#W" className="sidebar-text text-white">W</a></p>
+                        <p className="my-0"><a href="#X" className="sidebar-text text-white">X</a></p>
+                        <p className="my-0"><a href="#Y" className="sidebar-text text-white">Y</a></p>
+                        <p className="my-0"><a href="#Z" className="sidebar-text text-white">Z</a></p>
+                    </div>
+                </div>
+            </Row>
         </Container>
     )
 }
 
-export default CollectionNavigation;
+export default Collections;
