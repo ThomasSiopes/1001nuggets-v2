96e75853abb6c59dd2c549c0e0cc413c24c9e044
theeshyguy <t.siopes00@yahoo.com>
2023-05-02 13:56:48 -0400

    functional somePeople testing

--- client/src/pages/SomePeople.js
+++ client/src/pages/SomePeople.js
@@ -8,6 +8,7 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import QuoteCard from "../components/QuoteCard";
 
 import { QUERY_QUOTE_ALL } from "../utils/queries";
+import shuffle from "../utils/shuffle";
 
 function SomePeople () {
     let {loading, data} = useQuery(QUERY_QUOTE_ALL);
@@ -16,24 +17,34 @@ function SomePeople () {
 
     if(!data) return (<Redirect to={`/404error`}/>);
 
-    const quoteList = [...data.quotes];
+    const quoteList = data.quotes;
 
-    // let list1 
-    // let list2
-    // let list3a
-    // let list3b
-    // if(quoteList) {
-    //     let result = [];
-    //     for(let i = 3; i > 0; --i) {
-    //         result.push(quoteList.splice(0, Math.ceil(quoteList.length / i)))
-    //     }
-    //     list1 = result[0];
-    //     list2 = result[2];
+    let indexList = [];
+
+    for(let n = 0; n < quoteList; ++n) {
+        if(quoteList[n].somePeople) indexList.push(n);
+    }
+
+    if(!indexList || indexList == []) return <p>Loading...</p>
+
+    indexList = shuffle(indexList);
+
+    let list1 
+    let list2
+    let list3a
+    let list3b
+    if(quoteList) {
+        let result = [];
+        for(let i = 3; i > 0; --i) {
+            result.push(indexList.splice(0, Math.ceil(indexList.length / i)))
+        }
+        list1 = result[0];
+        list2 = result[2];
         
-    //     let middleIndex = Math.ceil(result[1].length/2);
-    //     list3a = result[1].splice(0,middleIndex);
-    //     list3b = result[1].splice(-middleIndex);
-    // }
+        let middleIndex = Math.ceil(result[1].length/2);
+        list3a = result[1].splice(0,middleIndex);
+        list3b = result[1].splice(-middleIndex);
+    }
 
     return (
         <Container>
@@ -45,12 +56,12 @@ function SomePeople () {
                 <Card.Body>
                     <Row>
                          {/* First Quote Column */}
-                         {/* <Col xs={12} md={6} lg={4}>
+                         <Col xs={12} md={6} lg={4}>
                             {list1 && 
                                 <Row>
                                     {list1.map((index) => (
-                                        <Col xs={12} className="mb-3" key={index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
@@ -59,21 +70,21 @@ function SomePeople () {
                                 list3a &&
                                 <Row className="d-xs-block d-lg-none">
                                     {list3a.map((index) => (
-                                        <Col xs={12} className="mb-3" key={"false" + index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={"false" + quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
                             }
-                        </Col> */}
+                        </Col>
 
                         {/* Middle Quote Column */}
-                        {/* <Col lg={4} className="d-none d-lg-block">
+                        <Col lg={4} className="d-none d-lg-block">
                             {list3a && 
                                 <Row>
                                     {list3a.map((index) => (
-                                        <Col xs={12} className="mb-3" key={index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
@@ -81,22 +92,22 @@ function SomePeople () {
                             {list3b && 
                                 <Row>
                                     {list3b.map((index) => (
-                                        <Col xs={12} className="mb-3" key={index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
                             }
-                        </Col> */}
+                        </Col>
                         
                         {/* Last Quote Column */}
-                        {/* <Col xs={12} md={6} lg={4}>
+                        <Col xs={12} md={6} lg={4}>
                             {
                                 list3b &&
                                 <Row className="d-xs-block d-lg-none">
                                     {list3b.map((index) => (
-                                        <Col xs={12} className="mb-3" key={"false" + index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={"false" + quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
@@ -104,13 +115,13 @@ function SomePeople () {
                             {list2 && 
                                 <Row>
                                     {list2.map((index) => (
-                                        <Col xs={12} className="mb-3" key={index.quoteText}>
-                                            <QuoteCard quotes={quoteList} quoteIndex={quoteList.indexOf(index)}/>
+                                        <Col xs={12} className="mb-3" key={quoteList[index].quoteText}>
+                                            <QuoteCard quotes={quoteList} quoteIndex={index} indexOrder={list1.concat(list3a.concat(list3b.concat(list2)))}/>
                                         </Col>
                                     ))}
                                 </Row>
                             }
-                        </Col> */}
+                        </Col>
                     </Row>
                 </Card.Body>
             </Card>
