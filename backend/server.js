03190c203fc91d0c88e8b59aa5c09b72003c0171
theeshyguy <t.siopes00@yahoo.com>
2024-01-31 14:24:50 -0500

    reverted to pre-SSR-test server style

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-const fs = require("fs")
 
 const { typeDefs, resolvers } = require("./mongo/schemas");
 
@@ -28,56 +27,16 @@ async function middleWare() {
 
 middleWare();
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 }
 
-app.get("/", function(req, response) {
-    console.log("Home page visited.")
-    const filePath = path.resolve(__dirname, "../frontend/build", "index.html");
-    
-    fs.readFile(filePath, "utf8", function(err, data) {
-        if(err) {
-            return console.log(err);
-        }
-        result = data.replace(/\$TWITTER_DESCRIPTION/g, "The application 1001nuggets!");
-        response.send(result);
-    })
-})
-
-app.get("/topics", function(req, response) {
-    console.log("Topics page visited.")
-    const filePath = path.resolve(__dirname, "../frontend/build", "topics.html");
-    
-    fs.readFile(filePath, "utf8", function(err, data) {
-        if(err) {
-            return console.log(err);
-        }
-        result = data.replace(/\$TWITTER_DESCRIPTION/g, "This is the topics page.")
-        response.send(result);
-    })
-})
-
-app.get("/quote", function(req, response) {
-    console.log("Quote page visited.")
-    const filePath = path.resolve(__dirname, "../frontend/build", "index.html");
-    
-    fs.readFile(filePath, "utf8", function(err, data) {
-        if(err) {
-            return console.log(err);
-        }
-        result = data.replace(/\$TWITTER_DESCRIPTION/g, "This is a quote page.")
-        response.send(result);
-    })
-})
-
-app.use(express.static("../frontend/build"));
-
-app.get("*", function(req, response) {
-    response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
 db.once("open", () => {
