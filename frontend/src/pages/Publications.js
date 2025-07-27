d45bb0c30f89c050ddd8afd4066b0cfe4c8c0be3
Thomas Siopes <t.siopes00@yahoo.com>
2025-07-27 14:31:46 -0400

    begun optimization of publication page

--- frontend/src/pages/Publications.js
+++ frontend/src/pages/Publications.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Container, Card, Row, Col, Button } from "react-bootstrap";
-import icon1 from "../assets/images/bookpics/book1.png";
-import icon2 from "../assets/images/bookpics/book2.png";
-import icon3 from "../assets/images/bookpics/book3.png";
-import icon4 from "../assets/images/bookpics/book4.png";
-import icon5 from "../assets/images/bookpics/book5.png";
-import icon6 from "../assets/images/bookpics/book6.png";
-import iconLite from "../assets/images/bookpics/bookLite.png";
-import iconSB from "../assets/images/bookpics/bookSB.png";
+import icon1 from "../assets/images/bookpics/book1.webp";
+import icon2 from "../assets/images/bookpics/book2.webp";
+import icon3 from "../assets/images/bookpics/book3.webp";
+import icon4 from "../assets/images/bookpics/book4.webp";
+import icon5 from "../assets/images/bookpics/book5.webp";
+import icon6 from "../assets/images/bookpics/book6.webp";
+import iconLite from "../assets/images/bookpics/bookLite.webp";
+import iconSB from "../assets/images/bookpics/bookSB.webp";
 
 function Publications() {
     return(
@@ -19,60 +19,36 @@ function Publications() {
             <Card>
                 <Card.Body>
                     <Row className="text-center">
-                        {/* <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 1</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 2</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 3</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 4</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 5</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 6</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Lite</Button>
-                        </Col>
-                        <Col className="my-2" xs={6} sm={4}>
-                            <Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Scrapbook</Button>
-                        </Col> */}
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon1}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 1"><Card.Img alt="Book Icon 1" variant="top" src={icon1}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 1</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon2}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 2"><Card.Img alt="Book Icon 2" variant="top" src={icon2}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 2</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon3}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 3"><Card.Img alt="Book Icon 3" variant="top" src={icon3}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 3</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon4}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 4"><Card.Img alt="Book Icon 4" variant="top" src={icon4}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 4</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon5}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 5"><Card.Img alt="Book Icon 5" variant="top" src={icon5}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 5</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={icon6}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book 6"><Card.Img alt="Book Icon 6" variant="top" src={icon6}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Book 6</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={iconLite}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for book lite"><Card.Img alt="Book Icon Lite" variant="top" src={iconLite}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Lite</Button></Card.Body>
                         </Card></Col>
                         <Col xs={12} sm={6} className="my-2"><Card>
-                                <a href="https://www.google.com"><Card.Img variant="top" src={iconSB}/></a>
+                                <a href="https://www.google.com" aria-label="Shop for scrapbook"><Card.Img alt="Book Icon Scrapbook" variant="top" src={iconSB}/></a>
                                 <Card.Body><Button className="btn-block" href={"https://www.google.com"} variant={"theme"}>1001 Nuggets: Scrapbook</Button></Card.Body>
                         </Card></Col>
                     </Row>
