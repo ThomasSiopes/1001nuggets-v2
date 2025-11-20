aa22cac036faa3ac3732d2651b9f39216c0534b6
Thomas Siopes <t.siopes00@yahoo.com>
2025-11-20 12:52:14 -0500

    testing with ad sizes

--- frontend/src/components/AdComponent/index.js
+++ frontend/src/components/AdComponent/index.js
@@ -8,11 +8,11 @@ class AdComponent extends React.Component {
     render () {
         return (
             <div>
-                <ins className="adsbygoogle"
+                <ins className="adsbygoogle ad-responsive"
                     style={{display:"block"}}
                     data-ad-client="ca-pub-7531037282217434"
                     data-ad-slot="2448754308"
-                    data-ad-format="auto, horizontal"
+                    data-ad-format="horizontal"
                     data-full-width-responsive="true"
                 />
             </div>
