e50c94d49dcdc7192fd015396cad75051e338b02
Thomas Siopes <t.siopes00@yahoo.com>
2026-02-27 15:28:09 -0500

    testing new lazy loading style with topic nav

--- frontend/src/components/TopicNavInst/index.js
+++ frontend/src/components/TopicNavInst/index.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-
+import { useLazyQuery } from "@apollo/client";
 import { QUERY_TOPIC_LETTER } from "../../utils/queries";
 
-function TopicNavInst({letter}) {
-    let {loading, data} = useQuery(QUERY_TOPIC_LETTER, {variables: {letter: letter},});
+function TopicNavInst({ letter }) {
+    const ref = useRef(null);
+    const [fetchLetter, { loading, data }] = useLazyQuery(QUERY_TOPIC_LETTER, {
+        variables: { letter },
+    });
 
-    if(loading) return <span>Loading {letter}s...</span>
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => { if (entry.isIntersecting) { fetchLetter(); observer.disconnect(); } },
+            { rootMargin: "200px" } // start fetching 200px before it enters view
+        );
+        if (ref.current) observer.observe(ref.current);
+        return () => observer.disconnect();
+    }, [fetchLetter]);
 
-    if(!data) return <span>...</span>;
+    if (loading) return <div ref={ref}><span>Loading {letter}s...</span></div>;
+    if (!data)   return <div ref={ref} />;
 
     let sortedList = data.topicLetter.map((item) => Object.assign({}, item, {selected:false}));
     
@@ -22,7 +32,7 @@ function TopicNavInst({letter}) {
     if(sortedList.length === 0) return <span/>
 
     return(
-        <div className="text-center mb-2">
+        <div ref={ref} className="text-center mb-2">
             <div>
                 <strong id={letter}>{letter.toUpperCase()}</strong>
                 <hr/>
