ea79165000f9ef1e0ce0293fd6e95c3ec2fe438c
Thomas Siopes <t.siopes00@yahoo.com>
2026-03-11 10:22:35 -0400

    fixed minor model issue

--- backend/mongo/models/Glossary.js
+++ backend/mongo/models/Glossary.js
@@ -13,7 +13,7 @@ const glossary = new Schema({
     content: [
         {
             index: {type: String, required:true, unique:true},
-            typing: {type: String, required:true, unique:true},
+            typing: {type: String, required:true, unique:false},
             definition: {type: String, required:true},
             sortedName: {type: String, required:true, unique:true},
         }
