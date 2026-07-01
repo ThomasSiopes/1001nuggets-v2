da19ea865b87d6997ae83de0d7b69ce6be0a8807
Thomas Siopes <t.siopes00@yahoo.com>
2026-07-01 13:24:33 -0400

    removed all 1-quote authors from nav

--- frontend/src/components/AuthorNavInst/index.js
+++ frontend/src/components/AuthorNavInst/index.js
@@ -1,51 +1,47 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
-import { useLazyQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { QUERY_AUTHOR_LETTER } from "../../utils/queries";
+// import LoadingOverlay from "../../components/LoadingOverlay";
 
 function AuthorNavInst({ letter }) {
     const ref = useRef(null);
-    const [fetchLetter, { loading, data }] = useLazyQuery(QUERY_AUTHOR_LETTER);
+    const { loading, data } = useQuery(QUERY_AUTHOR_LETTER, {
+        variables: {letter:letter}, fetchPolicy: "cache-and-network"
+    });
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            ([entry]) => { if (entry.isIntersecting) { fetchLetter({variables: { letter }}); observer.disconnect(); } },
-            { rootMargin: "200px" } // start fetching 200px before it enters view
-        );
-        if (ref.current) observer.observe(ref.current);
-        return () => observer.disconnect();
-    }, [fetchLetter, letter]);
-
-    if (loading) return <div ref={ref}><span>Loading {letter}s...</span></div>;
+    if (loading && !data) return <div ref={ref}>
+        {/* <LoadingOverlay show={loading && !data} /> */}
+        <span>Loading {letter}s...</span>
+        </div>;
     if (!data)   return <div ref={ref} />;
 
-    let sortedList = data.authorLetter.map((item) => Object.assign({}, item, {selected:false}))
+    let sortedList = data.authorLetter.map((item) => Object.assign({}, item, {selected:false}));
     
     sortedList = sortedList.sort(function(a,b) {
-        if(a.lastName < b.lastName) return -1;
-        if(a.lastName > b.lastName) return 1;
+        if(a.sortedName < b.sortedName) return -1;
+        if(a.sortedName > b.sortedName) return 1;
         return 0;
     });
 
-    let uglyList = []
-    for(let i of sortedList) if(i.quotes.length === 0) uglyList.push(i.name);
-    console.log("Letter " + letter + ": " + uglyList);
-    if(sortedList.length === 0) return <span/>;
+    if(sortedList.length === 0) return <span/>
 
     return(
+        <>
         <div ref={ref} className="text-center mb-2">
             <div>
                 <strong id={letter}>{letter.toUpperCase()}</strong>
                 <hr/>
             </div>
             {sortedList.map((index) => (
-                <div key={letter + index.name}>
+                <span key={letter + index.name}>{index.quotes.length > 1 && <div>
                     <p><strong><Link to={`/author/${index.realID}`} className="link-theme">{index.name}</Link></strong></p>
-                    {/* <p>({index.quotes.length} quotes)</p> */}
                     <hr/>
-                </div>
+                </div>}
+                </span>
             ))}
         </div>
+        </>
     );
 }
 
