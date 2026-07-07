ed8be98d1a49167ade119f97f6dc1cae8a7be131
Thomas Siopes <t.siopes00@yahoo.com>
2026-07-07 15:53:14 -0400

    updated footer to be more responsive on mobile

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -9,32 +9,34 @@ function Footer() {
     return(
         <footer id="footer"className="bg-light">
             <Container>
-                <Row className="py-4">
-                    <Col xs={12} md={2}>
-                        <Card border={"none"} bg={"transparent"} className="pt-4">
-                            <Container className="text-center">
-                                <a href="/"><Card.Img id="footer-icon" src={require("../../assets/images/N_IconGold.png")} alt="Logo"/></a>
-                            </Container>
-                            <Card.Body className="text-center px-1">
+                <Row className="pb-3">
+                    <Col xs={12} sm={5} md={2}>
+                        <Row className="pt-4 text-center align-items-center justify-content-center">
+                            <Col xs={3} sm={12}>
+                                <Container>
+                                    <a href="/"><Card.Img id="footer-icon" src={require("../../assets/images/N_IconGold.png")} alt="Logo"/></a>
+                                </Container>
+                            </Col>
+                            <Col xs={4} sm={12}>
                                 <a className="share-button mx-1 share-facebook" href="https://www.facebook.com/profile.php?id=61573520023726" target="_blank" rel="noopener noreferrer"><FaFacebook/></a>
                                 <a className="share-button mx-1 share-X" href="https://x.com/1001_Nuggets" target="_blank" rel="noopener noreferrer"><FaXTwitter/></a>
                                 <a className="share-button mx-1 share-instagram" href="https://www.instagram.com/1001_nuggets/" target="_blank" rel="noopener noreferrer"><FaInstagram/></a>
-                            </Card.Body>
-                        </Card>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col xs={12} md={3}>
+                    <Col md={3} className="d-none d-md-block">
                         <Container className="pt-4 mb-1">1001 Nuggets provides tens of thousands of wise quotes from authors, philosophers, and leaders, categorized by thousands of curated topics.</Container>
                     </Col>
-                    <Col xs={12} md={4} className="text-center">
+                    <Col sm={4} className="d-none d-sm-block text-center">
                         <p className="pt-4 mb-1">Site</p>
                         <Row>
-                            <Col xs={12} md={6}>
+                            <Col sm={12} md={6}>
                                 <p className="mb-0"><Link to="/">Home</Link></p>
                                 <p className="mb-0"><Link to="/authors">Authors</Link></p>
                                 <p className="mb-0"><Link to="/topics">Topics</Link></p>
                                 <p className="mb-0"><Link to="/collections">Collections</Link></p>
                             </Col>
-                            <Col xs={12} md={6}>
+                            <Col sm={12} md={6}>
                                 <p className="mb-0"><Link to="/author-catalogue">Author Catalogue</Link></p>
                                 <p className="mb-0"><Link to="/quote-catalogue">Quote Catalogue</Link></p>
                                 <p className="mb-0"><Link to="/glossary">Glossary</Link></p>
@@ -42,7 +44,7 @@ function Footer() {
                             </Col>
                         </Row>
                     </Col>
-                    <Col xs={12} md={3} className="text-center">
+                    <Col sm={3} className="d-none d-sm-block text-center">
                         <p className="pt-4 mb-1">About Us</p>
                         <p><Link to={"https://www.graffitian.com"}>Graffitian Publishing</Link></p>
                     </Col>
